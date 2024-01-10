0e0ce21dc51dd0cce116510d7a2a6aaa482738a3
Rahul <rahulshermale50@gmail.com>
2024-01-10 13:46:20 +0530

    Hello this is Second commite

--- Hello.java
+++ Hello.java
@@ -6,6 +6,14 @@ public static void main(String[]args){
 
 System.out.println("Hello Shubham");
 
+System.out.println("Hello Shubham");
+
+for(int i =1;i<=10;i++)
+{
+
+System.out.println("Hello Shubham");
+}
+
 }
 
 
