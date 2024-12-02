147771f09008be7851828bf9089cac6726fa722c
rahulshermale <support@bayanattechnology.com>
2024-12-02 11:59:36 +0530

    Commite

--- Hello.java
+++ Hello.java
@@ -3,11 +3,10 @@ public class Hello
 
 public static void main(String[]args){
 
+System.out.println("Loop Shubham");
 
-System.out.println("Hello Shubham");
 
-
-for(int i =1;i<=10;i++)
+for(int i =1;i<=5;i++)
 {
 
 System.out.println("Hello Shubham");
