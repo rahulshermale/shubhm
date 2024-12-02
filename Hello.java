5b4deb44b143b24a5b5032487432ac9f0c36a515
rahulshermale <rahulshermale50@gmail.com>
2024-12-02 12:06:53 +0530

    Commite

--- Hello.java
+++ Hello.java
@@ -3,13 +3,13 @@ public class Hello
 
 public static void main(String[]args){
 
-System.out.println("Loop Shubham");
+System.out.println("Loop Started Rahul");
 
 
 for(int i =1;i<=5;i++)
 {
 
-System.out.println("Hello Shubham");
+System.out.println("Hello Rahul");
 
   
 }
